1ff488d1476832ca6ea45ac0a3006b7b14102f5a
yfaheid <131709066+yfaheid@users.noreply.github.com>
2026-05-04 12:52:36 -0500

    add reset command

--- handler_user.go
+++ handler_user.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"fmt"
-	"os"
 	"time"
 
 	"github.com/google/uuid"
@@ -16,7 +15,6 @@ func handlerLogin(s *state, cmd command) error {
 	}
 	_, err := s.db.GetUser(context.Background(), cmd.args[0])
 	if err != nil {
-		os.Exit(1)
 		return err
 	}
 	err = s.cfg.SetUser(cmd.args[0])
@@ -33,7 +31,6 @@ func handlerRegister(s *state, cmd command) error {
 	}
 	newUser, err := s.db.CreateUser(context.Background(), database.CreateUserParams{ID: uuid.New(), CreatedAt: time.Now(), UpdatedAt: time.Now(), Name: cmd.args[0]})
 	if err != nil {
-		os.Exit(1)
 		return err
 	}
 
@@ -43,5 +40,16 @@ func handlerRegister(s *state, cmd command) error {
 	}
 	fmt.Println("user set to " + cmd.args[0])
 	fmt.Println(newUser)
+	return nil
+}
+
+func handlerReset(s *state, cmd command) error {
+	if len(cmd.args) != 0 {
+		return fmt.Errorf("incorrect amount of args")
+	}
+	err := s.db.DeleteUsers(context.Background())
+	if err != nil {
+		return err
+	}
 	return nil
 }
--- main.go
+++ main.go
@@ -31,6 +31,7 @@ func main() {
 	}
 	cmds.register("login", handlerLogin)
 	cmds.register("register", handlerRegister)
+	cmds.register("reset", handlerReset)
 	if len(os.Args) < 2 {
 		fmt.Println("no command")
 		os.Exit(1)
